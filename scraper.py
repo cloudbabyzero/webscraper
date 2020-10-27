d436d321e9f170929c77ebd4a36f5fe53e04a5c6
cloudbabyzero <cloudbabyzero@gmail.com>
2020-10-27 16:34:23 +0700

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -1,17 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 ##import mysql.connector
+number = 0
+while number < 10:
+    URL = 'https://th.indeed.com/jobs?q=software+engineer&l=Thailand&start=' + str(number)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content,'html.parser')
 
-URL = 'https://th.indeed.com/jobs?q=software+engineer&l=Thailand'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content,'lxml')
+    results = soup.find_all('div',attrs={'data-tn-component':'organicJob'})
+    for job_data in results:
+        company = job_data.find('span',attrs={"class":"company"})
+        company_final = company.text.strip()
+        location = job_data.find('span',attrs={"class":"location accessible-contrast-color-location"})
+        location_final = location.text.strip()
+        job = job_data.find('a',attrs={"data-tn-element":"jobTitle"})
+        job_final = job.text.strip()
+##  salary = job_data.find('span',attrs={"class":" })
+##  salary_final = salary.text.strip()
 
-results = soup.find_all('div',attrs={'data-tn-component':'organicJob'})
-for job_data in results:
-    company = job_data.find('span',attrs={"class":"company"})
-    company_final = company.text.strip()
-    job = job_data.find('a',attrs={"data-tn-element":"jobTitle"})
-    job_final = job.text.strip()
-
-    print(company_final)
-    print(job_final)
+        print(company_final)
+        print(job_final)
+        print(location_final)
+##  print(salary_final)
